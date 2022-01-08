b4d61f48a72143b3c6edecdb03dc07bfbb03f319
SIVAJI KADAPALA <83819803+sivaji-Kadapala@users.noreply.github.com>
2022-01-08 12:10:34 +0530

    Validations

---
+++ src/main/java/com/springboot/service/UserService.java
@@ -0,0 +1,19 @@
+package com.springboot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.model.User;
+import com.springboot.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User createUser(User user) {
+		return userRepository.save(user);
+	}	
+
+}
